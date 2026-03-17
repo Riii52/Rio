ea9246f9df129968af2d73c47b2c79da7c5a8e13
Riii52 <strawberry2011d@gmail.com>
2026-03-17 03:10:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ DATA_FILE = 'xp_data.json'
 GIF_URL = "https://i.ibb.co/aec358ce/flower-image.gif"
 LINE_URL = "https://cdn.discordapp.com/attachments/1458508427054813245/1459190184200503387/image.png"
 
-# الرتب التلقائية
+# الرتب التلقائية للـ Runs
 ROLES = {1: 1483227597801521316, 4: 1483227645016674384, 6: 1483227734728773784, 8: 1483227747563081841, 9: 1483227757981859950, 12: 1483227765669888092, 14: 1483227776638255307, 16: 1483227784737460225, 20: 1483227826718249031}
 
 def load_data():
@@ -64,14 +64,16 @@ async def on_message(msg):
     uid = str(msg.author.id)
     if uid not in data: data[uid] = {'xp': 0, 'runs': 0}
     
-    # تفاعل XP و لفل عادي
+    # تفاعل XP و إيمبد لفل جديد
     old_l, _, _ = xp_lvl_info(data[uid].get('xp', 0))
     data[uid]['xp'] = data[uid].get('xp', 0) + random.randint(15, 25)
     new_l, _, _ = xp_lvl_info(data[uid]['xp'])
     
     if new_l > old_l:
         l_ch = discord.utils.get(msg.guild.text_channels, name='·𓈒⟡⌇المستوى⋆₊˚⋆') or msg.channel
-        await l_ch.send(f"مبروك {msg.author.mention} وصلت لفل {new_l} في السيرفر")
+        e = discord.Embed(title="🎉 لفل جديد!", description=f"ماشاء الله {msg.author.mention} شاد حيلك اليوم! وصلت لفل ({new_l}) استمر نبي نشوفك دايم بالتوب ! ❤️", color=0xffc0cb)
+        e.set_thumbnail(url=msg.author.display_avatar.url)
+        await l_ch.send(content=f"ترقيت! {msg.author.mention}", embed=e)
 
     # رصد الرنز (Total Runs)
     if msg.channel.id == 1450459023617949747 and msg.mentions:
@@ -89,7 +91,7 @@ async def on_message(msg):
     save_data(data)
     await bot.process_commands(msg)
 
-@bot.command(name='rank', aliases=['r', 'R!'])
+@bot.command(name='rank', aliases=['r', 'R'])
 async def rank(ctx, m: discord.Member = None):
     m = m or ctx.author
     data = load_data()
@@ -97,8 +99,7 @@ async def rank(ctx, m: discord.Member = None):
     lvl, cur_xp, n_xp = xp_lvl_info(d['xp'])
     sorted_xp = sorted(data.items(), key=lambda x: x[1].get('xp', 0), reverse=True)
     rank_pos = next(i for i, (uid, _) in enumerate(sorted_xp, 1) if uid == str(m.id))
-    
-    e = discord.Embed(title=m.name, description=f"level\n{lvl}\ntotal XP\n{d['xp']}\n\nترتيبك بالسيرفر\n#{rank_pos}\n\nلفلك سيكون {lvl+1}\nيبي لك {n_xp-cur_xp} اكس بي عشان تصير اللفل {lvl+1}!", color=0xffc0cb)
+    e = discord.Embed(title=m.name, description=f"level\n{lvl}\ntotal XP\n{d['xp']}\n\nترتيبك بالسيرفر\n#{rank_pos}\n\nلفلك سيكون {lvl+1}\nيبي لك {n_xp-cur_xp} اكس بي عشان تصير اللفل {lvl+1}!\nشد حيلك🤍", color=0xffc0cb)
     e.set_thumbnail(url=m.display_avatar.url)
     e.set_footer(text=f"طلب بواسطة {ctx.author.display_name}")
     await ctx.send(embed=e)
@@ -114,7 +115,7 @@ async def toonrank(ctx, m: discord.Member = None):
     e.set_image(url=GIF_URL)
     await ctx.send(embed=e)
 
-@bot.command(name='Toptoons', aliases=['tt'])
+@bot.command(name='Toptoons', aliases=['tt!'])
 async def tt(ctx):
     sd = sorted(load_data().items(), key=lambda x: x[1].get('runs', 0), reverse=True)[:10]
     txt = "\n".join([f"#{i} | <@{u}> - {d.get('runs',0)} Runs" for i, (u, d) in enumerate(sd, 1)])
